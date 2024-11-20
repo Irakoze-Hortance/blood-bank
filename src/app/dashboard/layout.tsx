0ab3713eed99e53e93ed47f44eea9f6e2fb5d96a
felixdusengimana <phelixdusengimana@gmail.com>
2024-11-20 22:37:20 +0200

    top tear

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,4 +1,5 @@
 import { Sidebar } from "@/components/Sidebar/Sidebar";
+import { Topbar } from "@/components/Sidebar/Topbar";
 
 function DashboardLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -11,23 +12,7 @@ function DashboardLayout({ children }: { children: React.ReactNode }) {
       {/* Main Content */}
       <div className="flex-1 flex flex-col gap-14 overflow-y-auto bg-gray-100 px-8">
         {/* Fixed Main Navbar */}
-        <div className="h-16 sticky top-5 bg-white p-4 rounded-lg shadow-sm border border-gray-100 flex justify-between items-center">
-          <div>
-            <h2 className="text-2xl font-semibold">Volunteer</h2>
-            <p>Dashboard</p>
-          </div>
-          <div className="flex gap-3">
-            <div className="text-right">
-              <h2 className="font-bold">Julia Hanhrie</h2>
-              <p className="font-light">Active</p>
-            </div>
-            <img
-              src="https://picsum.photos/id/237/200/300"
-              alt="Profile"
-              className="w-10 h-10 rounded-full"
-            />
-          </div>
-        </div>
+        <Topbar />
         {/* Scrollable Main Content */}
         <div className="bg-gray-100">{children}</div>
       </div>
--- src/components/Sidebar/Topbar.tsx
+++ src/components/Sidebar/Topbar.tsx
@@ -1,13 +1,23 @@
 "use client";
 
-export const Topbar=()=>{
-    return(
-        <div className="flex justify-between items-center p-3  mt-4">
-            <h1 className="text-2xl font-bold">Volunteer</h1>
-            <div className="flex items-center">
-                <div className="mr-3">Julia Hahrie</div>
-                <div className="bg-primary rounded-full w-10 h-10 flex justify-center items-center text-white">JD</div>
-            </div>
+export const Topbar = () => {
+  return (
+    <div className="h-16 sticky top-5 bg-white p-4 rounded-lg shadow-sm border border-gray-100 flex justify-between items-center">
+      <div>
+        <h2 className="text-2xl font-semibold">Volunteer</h2>
+        <p>Dashboard</p>
+      </div>
+      <div className="flex gap-3">
+        <div className="text-right">
+          <h2 className="font-bold">Julia Hanhrie</h2>
+          <p className="font-light">Active</p>
         </div>
-    )
-}
+        <img
+          src="https://picsum.photos/id/237/200/300"
+          alt="Profile"
+          className="w-10 h-10 rounded-full"
+        />
+      </div>
+    </div>
+  );
+};
