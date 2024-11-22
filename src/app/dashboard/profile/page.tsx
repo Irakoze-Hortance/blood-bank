0a74ddfbe5e1d58640c1c8f71349b791203ce427
felixdusengimana <phelixdusengimana@gmail.com>
2024-11-22 10:42:16 +0200

    fix: profile link

--- src/app/dashboard/profile/page.tsx
+++ src/app/dashboard/profile/page.tsx
@@ -4,12 +4,8 @@ import { Topbar } from "@/components/Sidebar/Topbar";
 import { Sidebar } from "@/components/Sidebar/Sidebar";
 const Dashboard = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="flex-1 ml-64 p-6">
-        <Topbar />
-        <ProfilePage />
-      </div>
+    <div className="">
+      <ProfilePage />
     </div>
   );
 };
