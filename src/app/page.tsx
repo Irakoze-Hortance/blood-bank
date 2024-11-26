41096882124a8e3fda316bfb25b8b47747a4fff2
Ange-Constance <constance.nimuhire@gmail.com>
2024-11-26 18:05:46 +0200

    superadmin: dashboard, Donors,HospitalsPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 "use client";
-
-import MainPage from "@/components/superadmin/pages/MainPage";
-// import App from "next/app";
-
-// import { Landing } from "./Landing/Landing";
+import { Landing } from "./Landing/Landing";
 export default function Home() {
-  // return <Landing />;
-return MainPage
+  return <Landing />;
+
 }
